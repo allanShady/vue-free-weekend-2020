efc2a5514295421f732c386aeecbd0201d76b0ea
Alan Camilo <acamilo.ag@gmail.com>
2020-03-14 11:27:53 +0200

    usage of computed properties
    helps a lot when we have same operations to perform and show the result
    in html page.

--- into-2-vuejs/assets/script/main.js
+++ into-2-vuejs/assets/script/main.js
@@ -4,15 +4,15 @@ let app = new Vue({
     el: '#app',
     data: {
         cart: 0,
+        brand: 'Vue mastery',
         product: 'Socks',
-        image: 'assets/img/vmSocks-green-onWhite.jpg',
+        selectedVariantIndex: 0,
         altText: 'A pair of sucks',
         productInfo: 'https://brave-hugle-f2518f.netlify.com',
-        inStock: true,
         details: ['80% cottons', '20% polyester', 'Genders-neutral'],
         variants: [
-            {id: 1, color: 'green', image: 'assets/img/vmSocks-green-onWhite.jpg'},
-            {id: 2, color: 'blue', image: 'assets/img/vmSocks-blue-onWhite.jpg'},
+            {id: 1, color: 'green', image: 'assets/img/vmSocks-green-onWhite.jpg', quantity: 0},
+            {id: 2, color: 'blue', image: 'assets/img/vmSocks-blue-onWhite.jpg', quantity: 12},
         ],
         sizes:['S', 'M', 'L', 'XL', 'XXL']
     },
@@ -28,8 +28,22 @@ let app = new Vue({
                 --this.cart;
         },
 
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariantIndex = index;
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.brand} ${this.product}`;
+        },
+
+        image() {
+            return this.variants[this.selectedVariantIndex].image;
+        },
+
+        inStock() {
+            return this.variants[this.selectedVariantIndex].quantity
         }
     }
 });
