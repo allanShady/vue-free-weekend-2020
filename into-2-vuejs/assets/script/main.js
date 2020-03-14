eb8bebda5377bb1fc29b02eb3a7c3c03f1168702
Alan Camilo <acamilo.ag@gmail.com>
2020-03-14 09:21:40 +0200

    usage of v-for
    to display product details and its variants

--- into-2-vuejs/assets/script/main.js
+++ into-2-vuejs/assets/script/main.js
@@ -7,6 +7,12 @@ let app = new Vue({
         image: 'assets/img/vmSocks-green-onWhite.jpg',
         altText: 'A pair of sucks',
         productInfo: 'https://brave-hugle-f2518f.netlify.com',
-        inStock: 0
+        inStock: 0,
+        details: ['80% cottons', '20% polyester', 'Genders-neutral'],
+        variants: [
+            {id: 1, color: 'green'},
+            {id: 2, color: 'blue'},
+            {id: 3, color: 'red'}
+        ]
     }
 });
