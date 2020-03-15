8346bdde4487424a461ea77af19b20f83edc0c07
Alan Camilo <acamilo.ag@gmail.com>
2020-03-15 15:08:29 +0200

    usage of computed properties
    to mapp data from vuex state

--- real-world-app/src/store/index.js
+++ real-world-app/src/store/index.js
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {id: 1, name: 'Alan Camilo'},
+    categories: ['sport', 'gaming', 'programing']
+  },
   mutations: {},
   actions: {},
   modules: {}
--- real-world-app/src/views/EventCreate.vue
+++ real-world-app/src/views/EventCreate.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        <label> Create events </label>
+        <h4>Create an Event, {{user.name}}</h4>
+        <p>This event was created by: {{user.id}}</p>
+
+        <span>Categories</span>
+        <ul>
+            <li v-for="cat in categories" :key="cat"> {{cat}}</li>
+        </ul>
     </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    computed: {
+        
+        ...mapState(['user', 'categories'])
+    }
+}
+</script>
