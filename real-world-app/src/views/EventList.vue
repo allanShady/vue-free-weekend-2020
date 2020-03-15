a0dbf47a30a3d50d19d3f567eb3e3823c1272d58
Alan Camilo <acamilo.ag@gmail.com>
2020-03-15 13:38:41 +0200

    Fetch events with axios and present

--- real-world-app/src/views/EventList.vue
+++ real-world-app/src/views/EventList.vue
@@ -1,17 +1,33 @@
 <template>
     <div>
         <label>Event Listing</label>
-        <event-card/>
+        <event-card v-for="event in events" :key="event.id" :event="event" />
     </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import EventServices from '@/services/EventServices.js';
 
 export default {
     components: {
         EventCard
+    },
+
+    data() {
+        return {
+            events: []
+        }
+    },
+
+    created(){
+        EventServices.getEvents('/events')
+        .then(resp => {
+            this.events = resp.data
+        })
+        .catch(error => console.log(error));
     }
+    
 
 }
 </script>
